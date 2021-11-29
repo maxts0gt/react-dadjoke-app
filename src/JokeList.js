03fca8ed47cc061e296cc8fa9121dc44db6fa811
Max <bazarsadtsogtjargal@gmail.com>
2021-11-30 01:08:23 +0900

    sort jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -82,7 +82,7 @@ class JokeList extends React.Component {
 				</div>
 			);
 		}
-
+		let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
 		return (
 			<div className='JokeList'>
 				<div className='JokeList-sidebar'>
@@ -99,7 +99,7 @@ class JokeList extends React.Component {
 				</div>
 
 				<div className='JokeList-jokes'>
-					{this.state.jokes.map((j) => (
+					{jokes.map((j) => (
 						<Joke
 							key={j.id}
 							votes={j.votes}
